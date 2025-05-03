8409b2304dd1c49ba9896b22d83fd3ac2226d2e3
Ivanildo Felix <felixsilva104@gmail.com>
2025-05-03 17:13:32 -0300

    Tirando obrigatoriedade de senha no update

--- src/main/java/com/aquaone/srv/login/services/UserService.java
+++ src/main/java/com/aquaone/srv/login/services/UserService.java
@@ -53,7 +53,7 @@ public class UserService {
         existiUser.setDescription(data.description());
         existiUser.setAdmin(data.admin());
         existiUser.setEmail(data.email());
-        existiUser.setPassword(passwordEncoder.encode((data.password())));
+        existiUser.setPassword(data.password().length() > 0 ? passwordEncoder.encode((data.password())) : existiUser.getPassword());
         
         return userRepository.save(existiUser);
     }
